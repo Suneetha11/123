c774022671fcdf7c10ece1d1f7e73505758e9c0d
SuneethaAvvaru <suneethaavvaru@gmail.com>
2024-08-31 11:43:31 +0530

    Initial commit

---
+++ HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
